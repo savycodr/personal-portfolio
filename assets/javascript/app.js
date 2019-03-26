8def5b18115b63eac81dd1f96da237d5d04d3673
savycodr <46404719+savycodr@users.noreply.github.com>
2019-03-26 13:02:47 -0400

    changed media respnsiveness check from 400 to 450 px

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     $('.parallax').parallax(
         {
             // The minimum width of the screen, in pixels, where the parallax functionality starts working.
-            responsiveThreshold: 400
+            responsiveThreshold: 450
         });
     // a call to Materialize tap target
     $('.tap-target').tapTarget();
